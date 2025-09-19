26ed84f1a1b087a45ca09101f8f73d1cf69ebe83
Ivan Todinov <ivan.todinov@miracl.com>
2025-09-19 11:36:38 +0300

    Update search widget

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,64 +1,98 @@
-axios.get("/search")
-    .then(function (result) {
-        var searchContent = result.data;
-        var searchIndex = lunr(function () {
-            this.ref("id")
-            this.field("content");
-            this.field("tag");
-            this.field("title");
-            this.field("url");
+;(function () {
+    const searchTrigger = document.getElementById("search-trigger");
+    const searchModal = document.getElementById("search-modal");
+    const searchInput = document.getElementById("search-field");
+    const searchResultElement = document.getElementById("search-results");
 
-            Array.from(result.data).forEach(function (doc) {
-                this.add(doc)
-            }, this)
-        })
-
-        var searchWrapper = document.getElementById("search-wrapper");
-        var searchInput = document.getElementById("search-field");
-        var searchResultElement = document.getElementById("search-results");
-
-        searchInput.removeAttribute("disabled");
-
-        searchInput.addEventListener("input", function (e) {
-            var searchString = e.target.value;
-
-            if (!searchString) {
-                searchWrapper.classList.remove("is-active");
-                return;
-            }
-
-            var searchResults = [];
-
-            if (searchString && searchString.length > 2) {
-                try {
-                    searchResults = searchIndex.search(searchString);
-                } catch (err) {
-                    if (err instanceof lunr.QueryParseError) {
-                        return;
-                    }
-                }
-            }
-
-            searchWrapper.classList.add("is-active");
-
-            if (searchResults.length > 0) {
-                searchResultElement.innerHTML = searchResults.map(function (match) {
-                    var item = searchContent.find(function(e) {
-                        return e.id == parseInt(match.ref);
-                    });
-
-                    return [
-                        "<div class='dropdown-item'>",
-                        "<h4><a href='" + item.url + "'>" + item.title + "</a></h4>",
-                        "<p>" + (item.content.length > 100 ? (item.content.substring(0, 100) + "...") : item.content) + "</p>",
-                        "</div>"
-                    ].join("");
-                }).join("<hr class='navbar-divider'>");
-            } else {
-                searchResultElement.innerHTML = '<div class="dropdown-item"><p>No results found</p></div>';
-            }
-        });
-    })
-    .catch(function (error) {
-        console.error(error);
+    const searchIndex = new MiniSearch({
+        fields: ['title', 'content'], // fields to index for full-text search
+        storeFields: ['title', 'content', 'url', 'ancestors'], // fields to return with search results
+        searchOptions: {
+            boost: { title: 2 },
+            fuzzy: 0.2
+        }
     });
+
+    fetch("/search/index.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(result => {
+            searchIndex.addAll(result);
+            searchInput.removeAttribute("disabled");
+        });
+
+    searchInput.addEventListener("input", e => {
+        const searchString = e.target.value;
+
+        if (!searchString || searchString.length < 3) {
+            return;
+        }
+
+        const searchResults = searchIndex.search(searchString);
+
+        if (searchResults.length > 0) {
+            searchResultElement.innerHTML = searchResults.map(item => {
+                return `
+                    <div>
+                        <nav class="breadcrumb is-small mt-1 mb-0">
+                            <ul>
+                                ${item.ancestors.reduce((acc, val) => {
+                                    return acc + `<li><a>${val.title}</a></li>`
+                                }, '')}
+                                <li></li>
+                            </ul>
+                        </nav>
+                        <h6 class="mt-1 mb-1"><a href="${item.url}">${item.title}</a></h6>
+                        <p>${(item.content.length > 200 ? (item.content.substring(0, 200) + "...") : item.content)}</p>
+                    </div>
+                `;
+            }).join('<hr class="mt-3 mb-3">');
+        } else {
+            searchResultElement.innerHTML = '<p>No results found</p>';
+        }
+    });
+
+    searchTrigger.addEventListener("click", e => {
+        e.preventDefault();
+        openSearch();
+    });
+
+    document.addEventListener("keydown", e => {
+        switch (e.key) {
+        case "/":
+            if (!isSearchOpen()) {
+                e.preventDefault();
+                openSearch();
+            }
+            break;
+        case "Escape":
+            if (isSearchOpen()) {
+                e.preventDefault();
+                closeSearch();
+            }
+            break;
+        }
+    });
+
+    document.querySelectorAll('#search-modal .modal-background, #search-modal .modal-close').forEach(target => {
+        target.addEventListener('click', e => {
+            e.preventDefault();
+            closeSearch();
+        });
+    });
+
+    function openSearch() {
+        searchModal.classList.add("is-active");
+        searchInput.focus();
+    }
+
+    function closeSearch() {
+        searchModal.classList.remove("is-active");
+    }
+
+    function isSearchOpen() {
+        return searchModal.classList.contains("is-active");
+    }
+})();
+
